ea7aed625d039e3ab2de544d1a52531720317f90
LuisNivar <luisjosenivarn@hotmail.com>
2025-04-20 16:31:44 -0400

    Use Paymente Services in its endpoint

--- src/Application/Features/Payment/PaymentEndPoint.cs
+++ src/Application/Features/Payment/PaymentEndPoint.cs
@@ -1,97 +1,71 @@
-using System.Data;
 using Application.Domain.Enums;
+using Application.Domain.Interfaces;
+using Application.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
-using Stripe;
-using Stripe.Checkout;
 
 namespace Application.Features.Payment;
 
 public static class PaymentEndpoint
 {
-    //TODO: Move to service + interface
     public static void AddPaymentEndpoint(this IEndpointRouteBuilder api)
     {
         api.MapPost("/pay/{orderId:int}", PayOrder);
         api.MapPost("/webhook", UpdateStatus);
     }
 
-    private static async Task<IResult> PayOrder(int orderId, ApplicationDbContext dbContext, HttpContext httpContext)
+    private static async Task<Results<Ok<PaymentResult>, BadRequest>> PayOrder
+    (
+        int orderId, ApplicationDbContext dbContext,
+        PaymentService paymentService)
     {
-        var _domain = "http/frontend-fake";
+        var _domain = "http://localhost:5173"; //Supposed frontend
         var order = await dbContext.Orders
             .Include(o => o.Items)
             .FirstOrDefaultAsync(o => o.Id == orderId);
 
         if (order is null || order.IsPaid)
-            return Results.BadRequest("Order invalid!");
+            return TypedResults.BadRequest();
 
-        var lineItems = order.Items.Select(item => new SessionLineItemOptions
-        {
-            PriceData = new SessionLineItemPriceDataOptions
-            {
-                UnitAmountDecimal = item.UnitPrice * 100, // convertir a centavos
-                Currency = "usd",
-                ProductData = new SessionLineItemPriceDataProductDataOptions
-                {
-                    Name = item.ProductName
-                }
-            },
-            Quantity = item.Quantity
-        }).ToList();
-
-        var options = new SessionCreateOptions
-        {
-            PaymentMethodTypes = new List<string> { "card" },
-            Mode = "payment",
-            SuccessUrl = $"{_domain}/success?orderId={order.Id}",
-            CancelUrl = $"{_domain}/cancel",
-            LineItems = lineItems
-        };
-
-        var service = new SessionService();
-        var session = await service.CreateAsync(options);
-
-        return Results.Ok(new { sessionId = session.Id, url = session.Url });
+        var result = await paymentService.PayOrder(order, _domain);
+        return TypedResults.Ok(result);
     }
 
-    public static async Task<IResult> UpdateStatus(HttpRequest request, ApplicationDbContext db, IOptions<StripeOptions> options)
+    public static async Task<IResult> UpdateStatus(
+        HttpRequest request,
+        ApplicationDbContext db,
+        IPayment paymentService,
+        IOptions<StripeOptions> options
+        )
     {
         var json = await new StreamReader(request.Body).ReadToEndAsync();
+        Console.WriteLine(new { json });
         var stripeSignature = request.Headers["Stripe-Signature"];
-
         var endpointSecret = options.Value.WebhookSecret;
 
-        Event stripeEvent;
+        var result = paymentService.UpdateStatus(json, stripeSignature!, endpointSecret);
 
-        try
-        {
-            stripeEvent = EventUtility.ConstructEvent(json, stripeSignature, endpointSecret);
-        }
-        catch (Exception e)
-        {
-            return Results.BadRequest($"Webhook error: {e.Message}");
-        }
+        if (result.RequestResult == PaymentRequestResult.BadRequest)
+            return TypedResults.BadRequest();
 
-        //FIXME: Debug
-        Console.WriteLine(new { stripeEvent.Type });
+        if (result.RequestResult == PaymentRequestResult.NotFound)
+            return TypedResults.NotFound();
 
-        if (stripeEvent.Type == "checkout.session.completed")
-        {
-            var session = stripeEvent.Data.Object as Session;
-            var orderId = int.Parse(session?.SuccessUrl?.Split("orderId=")?.Last() ?? "0");
+        int orderId = result.OrderId;
+        var order = await db.Orders.FindAsync(orderId);
 
-            var order = await db.Orders.FindAsync(orderId);
-            if (order is not null)
-            {
-                order.IsPaid = true;
-                order.Status = OrderStatus.Completed;
-                await db.SaveChangesAsync();
-            }
-        }
+        if (order is null)
+            return Results.NotFound("Order not found!");
+
+        order.IsPaid = true;
+        order.Status = OrderStatus.Completed;
+
+        db.Orders.Update(order);
+        await db.SaveChangesAsync();
 
         return Results.Ok();
     }
