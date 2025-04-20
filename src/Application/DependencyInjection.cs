bbdcf1081467f5b570ef72beee92abec97e0ee01
LuisNivar <luisjosenivarn@hotmail.com>
2025-04-20 16:31:19 -0400

    - Add Kestrel config - Add Stripe Options - Register Payment Services

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -9,6 +9,7 @@ using Application.Services;
 using System.Text.Json.Serialization;
 using Stripe;
 using Application.Features.Payment;
+using Microsoft.AspNetCore.Hosting;
 
 namespace Application;
 
@@ -16,6 +17,12 @@ public static class DependencyInjection
 {
     public static void AddApplication(this WebApplicationBuilder builder)
     {
+        // Kestrel
+        builder.WebHost.ConfigureKestrel(options =>
+        {
+            options.Configure(builder.Configuration.GetSection("Kestrel"));
+        });
+
         // Database
         var DataBaseConnectionString = builder.Configuration.GetConnectionString("Database");
         builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -36,15 +43,28 @@ public static class DependencyInjection
         //  Orders 
         builder.Services.AddScoped<OrderService>();
 
-
         // Enum to String 
         builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =>
         {
             options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
         });
 
-        builder.Services.Configure<StripeOptions>(builder.Configuration.GetRequiredSection("Stripe"));
-        StripeConfiguration.ApiKey = builder.Configuration["Stripe:SecretKey"];
+        var stripeOptions = new StripeOptions
+        {
+            ApiKey = Environment.GetEnvironmentVariable("Stripe__ApiKey")!,
+            WebhookSecret = Environment.GetEnvironmentVariable("Stripe__WebhookSecret")!
+        };
+
+        builder.Services.Configure<StripeOptions>(options =>
+        {
+            options.ApiKey = stripeOptions.ApiKey;
+            options.WebhookSecret = stripeOptions.WebhookSecret;
+        });
+
+        StripeConfiguration.ApiKey = stripeOptions.ApiKey;
+
+        // Payment
+        builder.Services.AddScoped<IPayment, PaymentService>();
     }
 
 }
